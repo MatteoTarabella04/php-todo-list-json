2a37c514532b1dc4df28b0e9bd47b178083aaad5
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-05-08 15:37:42 +0200

    import bootstrap and fontawesome cdn

--- index.php
+++ index.php
@@ -4,6 +4,13 @@
 <head>
    <meta charset='utf-8'>
    <meta name='viewport' content='width=device-width, initial-scale=1'>
+   <!-- bs5 -->
+   <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css' rel='stylesheet'
+      integrity='sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65' crossorigin='anonymous'>
+   <!-- font awesome -->
+   <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css'
+      integrity='sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ=='
+      crossorigin='anonymous' referrerpolicy='no-referrer' />
    <title>ToDo List</title>
 </head>
 
@@ -14,9 +21,14 @@
 
    </div>
 
+   <!-- axios -->
    <script src='https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js'></script>
+   <!-- vue -->
    <script src='https://unpkg.com/vue@3/dist/vue.global.js'></script>
+   <!-- my JS -->
    <script src='./app.js'></script>
+   <!-- bs5 -->
+   <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js'></script>
 </body>
 
 </html>
