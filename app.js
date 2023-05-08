0e132234ffe467e426f77dd157d86019843ff825
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-05-08 16:43:28 +0200

    add function to add tasks

--- app.js
+++ app.js
@@ -1,23 +1,44 @@
-const { createApp } = Vue
+const { createApp } = Vue;
 
 createApp({
    data() {
       return {
-         api_url: './getTasks.php',
-      }
+         api_url: "./getTasks.php",
+         api_store_url: "./storeTasks.php",
+         tasks: [],
+         new_task: "",
+      };
    },
    methods: {
+      addTask() {
+         this.tasks.unshift(this.new_task);
+
+         const data = {
+            new_task: this.new_task,
+         };
+
+         axios
+            .post(this.api_store_url, data, {
+               headers: { "Content-Type": "multipart/form-data" },
+            })
+            .then((response) => {
+               console.log(response);
+            })
+            .catch((error) => {
+               console.error(error.message);
+            });
+         this.new_task = "";
+      },
    },
    mounted() {
-
       axios
          .get(this.api_url)
-         .then(response => {
+         .then((response) => {
             console.log(response);
+            this.tasks = response.data;
          })
-         .catch(error => {
+         .catch((error) => {
             console.error(error.message);
-         })
-   }
-
-}).mount('#app');
+         });
+   },
+}).mount("#app");
