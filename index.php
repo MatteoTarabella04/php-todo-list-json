6627323bce4e65f489309a094b4e74fdb58ef874
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-05-08 17:16:21 +0200

    add some style

--- index.php
+++ index.php
@@ -11,6 +11,8 @@
    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css'
       integrity='sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ=='
       crossorigin='anonymous' referrerpolicy='no-referrer' />
+   <!-- mystyle -->
+   <link rel="stylesheet" href="style.css">
    <title>ToDo List</title>
 </head>
 
@@ -19,14 +21,22 @@
 
 
       <div class="container">
-         <h1>ToDo List</h1>
+         <h1 class="text-white text-muted text-center">ToDo List</h1>
 
-         <ul>
-            <li v-for="task in tasks">
-               {{task}}
-            </li>
-         </ul>
-         <input class="form-control" type="text" placeholder="Add a task" v-model="new_task" @keyup.enter="addTask">
+         <div class="card border-0 w-25 m-auto">
+            <div class="my_list">
+               <ul class="list-group">
+                  <li class="list-group-item d-flex justify-content-between align-items-center" v-for="task in tasks">
+                     {{task}}
+                     <button class="btn bg-danger"><i class="fa fa-trash text-white" aria-hidden="true"></i></button>
+                  </li>
+               </ul>
+            </div>
+         </div>
+         <div class="input-group w-25 m-auto mt-3">
+            <input class="form-control" type="text" placeholder="Add a task" v-model="new_task" @keyup.enter="addTask">
+            <button class="btn btn-outline-info" type="button" @click="addTask">Add</button>
+         </div>
       </div>
 
 
