65dfe5e5898bf54c80b7efe653dfc79a13f1c49b
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-05-09 12:56:37 +0200

    add axios call for deleted tasks

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ createApp({
          api_index_url: "app/Http/Controllers/TasksController/index.php",
          api_store_url: "app/Http/Controllers/TasksController/store.php",
          api_update_url: "app/Http/Controllers/TasksController/updateTasks.php",
+         api_delete_url: "app/Http/Controllers/TasksController/deleteTasks.php",
          tasks: [],
          new_task: '',
       };
@@ -38,6 +39,23 @@ createApp({
 
          axios
             .post(this.api_update_url, data, {
+               headers: { "Content-Type": "multipart/form-data" },
+            })
+            .then((response) => {
+               console.log(response);
+               this.tasks = response.data;
+            })
+            .catch((error) => {
+               console.error(error.message);
+            });
+      },
+      deleteTask(i) {
+         const data = {
+            taskToDelete: i,
+         };
+
+         axios
+            .post(this.api_delete_url, data, {
                headers: { "Content-Type": "multipart/form-data" },
             })
             .then((response) => {
