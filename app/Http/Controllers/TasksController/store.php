3a926dffa592cfb8c82de3e51336385648c91273
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-05-09 12:32:24 +0200

    change array push to unshift

--- app/Http/Controllers/TasksController/store.php
+++ app/Http/Controllers/TasksController/store.php
@@ -3,21 +3,22 @@
 $json_path = '../../../../data/tasks.json';
 
 if (!empty($_POST['new_task'])) {
+
    $new_task = [
       'name' => ($_POST['new_task']),
-      'done' => false
+      'done' => false,
    ];
+
+   $tasks_string = file_get_contents($json_path);
+
+   $tasks_array = json_decode($tasks_string, true);
+
+   array_unshift($tasks_array, $new_task);
+
+   $new_tasks_string = json_encode($tasks_array);
+
+   file_put_contents($json_path, $new_tasks_string);
+
+   header('Content-Type: application/json');
+   echo $new_tasks_string;
 }
-
-$tasks_string = file_get_contents($json_path);
-
-$tasks_array = json_decode($tasks_string, true);
-
-array_push($tasks_array, $new_task);
-
-$new_tasks_string = json_encode($tasks_array);
-
-file_put_contents($json_path, $new_tasks_string);
-
-header('Content-Type: application/json');
-echo $new_tasks_string;
